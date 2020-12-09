af39c7dff203eb4654da823c93a0f9cd6c4fad7b
Soyras <74102984+Soyras@users.noreply.github.com>
2020-12-09 20:46:14 +0100

    Update botte_secrete.py

--- botte_secrete.py
+++ botte_secrete.py
@@ -1,9 +1,18 @@
+from random import randint
+
 def make_pair(names):
+    liste = []
     for i in range(len(names)-1):
-        print(f'Paire n°{i+1}: {names[i]}, {names[i+1]}')        
+        liste.append(names[i])
+        liste.append(names[i+1])
+    return liste
+
     
-def
+def split_names(names):
+    return names.split(", ")
 
 x = input('Entrez les noms des participant(e)s, séparés par des virgules: ')
 noms = x.split(", ")
 make_pair(noms)
+print()
+split_names(noms)
